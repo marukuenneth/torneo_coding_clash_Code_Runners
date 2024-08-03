a4d8f18a2fede47ea187a12cdc665ad04c270339
Maria Kuenneth <mariakuenneth@gmail.com>
2024-08-03 16:40:07 -0300

    ultimo commit de combinaciones de telefono

---
+++ final/Combinaciones_De_Telefono/combinacionesDeTelefono.js
@@ -0,0 +1,34 @@
+combinacion();
+function combinacion(){
+    const dos=["a","b","c"];
+    const tres=["d","e","f"];
+    const cuatro=["g","h","i"];
+    const cinco=["j","k","l"];
+    const seis=["m","n","o"];
+    const siete=["p","q","r","s"];
+    const ocho=["t","u","v"];
+    const nueve=["w","x","y","z"];
+    
+    let numero = 23;
+    // do {
+    //     numero = prompt('Ingresa un numero');
+    //     console.log(numero);
+    //     if (!isNaN(parseInt(numero)) && numero != null) {
+    //         console.log(numero);
+    //     } else {
+    //         if (numero != null) {
+    //             alert(numero + ' No es un numero');
+    //         }
+    //     }
+    // } while (isNaN((numero)));
+
+    numero.toString();
+    console.log(numero);
+    
+    for (let i = 0; i < numero.length; i++) {
+        const element = array[i];
+        if (palabra.includes("2")) {
+            
+        }
+    }
+}
